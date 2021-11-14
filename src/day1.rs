cab6cd997c23c85d9774376b5683e51d6bcf08eb
Matt Burke <spraints@gmail.com>
2021-11-13 22:06:31 -0500

    2015/1/2

--- src/day1.rs
+++ src/day1.rs
@@ -5,10 +5,22 @@ pub fn run<R: Read>(mut r: R) {
     let mut s = String::new();
     r.read_to_string(&mut s).unwrap();
     println!("end on floor: {}", part1(&s));
+    println!("basement at step: {}", part2(&s));
 }
 
 fn part1(s: &str) -> i32 {
     s.chars().fold(0, |floor, c| floor + dir(c))
+}
+
+fn part2(s: &str) -> usize {
+    let mut floor = 0;
+    for (i, c) in s.chars().enumerate() {
+        floor += dir(c);
+        if floor == -1 {
+            return i + 1;
+        }
+    }
+    0
 }
 
 fn dir(c: char) -> i32 {
@@ -25,4 +37,10 @@ fn test_part1() {
     assert_eq!(0, part1("()()"));
     assert_eq!(3, part1("((("));
     assert_eq!(3, part1("(()(()("));
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(1, part2(")"));
+    assert_eq!(5, part2("()())"));
 }
