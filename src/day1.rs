e7c9473391ad600e36ff98ec7cd15b65a3156ca3
Matt Burke <spraints@gmail.com>
2021-11-13 22:01:24 -0500

    2015/1/1

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,28 @@
 use super::common::read_lines::read_lines;
 use std::io::Read;
 
-pub fn run<R: Read>(r: R) {
-    for line in read_lines(r) {
-        println!("line: {}", line);
+pub fn run<R: Read>(mut r: R) {
+    let mut s = String::new();
+    r.read_to_string(&mut s).unwrap();
+    println!("end on floor: {}", part1(&s));
+}
+
+fn part1(s: &str) -> i32 {
+    s.chars().fold(0, |floor, c| floor + dir(c))
+}
+
+fn dir(c: char) -> i32 {
+    match c {
+        '(' => 1,
+        ')' => -1,
+        _ => 0,
     }
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(0, part1("(())"));
+    assert_eq!(0, part1("()()"));
+    assert_eq!(3, part1("((("));
+    assert_eq!(3, part1("(()(()("));
 }
