18adb186c389e2291dd7e0da85af44f4f19aa608
Matt Burke <spraints@gmail.com>
2021-12-01 00:07:39 -0500

    solve day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,46 +1,17 @@
-use super::common::read_lines::read_lines;
+use super::common::parse_lines::parse_lines;
 use std::io::Read;
 
 pub fn run<R: Read>(mut r: R) {
-    let mut s = String::new();
-    r.read_to_string(&mut s).unwrap();
-    println!("end on floor: {}", part1(&s));
-    println!("basement at step: {}", part2(&s));
-}
-
-fn part1(s: &str) -> i32 {
-    s.chars().fold(0, |floor, c| floor + dir(c))
-}
-
-fn part2(s: &str) -> usize {
-    let mut floor = 0;
-    for (i, c) in s.chars().enumerate() {
-        floor += dir(c);
-        if floor == -1 {
-            return i + 1;
+    let nums: Vec<u16> = parse_lines(r);
+    let mut last = None;
+    let mut count = 0;
+    for num in nums {
+        if let Some(n) = last {
+            if num > n {
+                count = count + 1;
+            }
         }
+        last = Some(num);
     }
-    0
-}
-
-fn dir(c: char) -> i32 {
-    match c {
-        '(' => 1,
-        ')' => -1,
-        _ => 0,
-    }
-}
-
-#[test]
-fn test_part1() {
-    assert_eq!(0, part1("(())"));
-    assert_eq!(0, part1("()()"));
-    assert_eq!(3, part1("((("));
-    assert_eq!(3, part1("(()(()("));
-}
-
-#[test]
-fn test_part2() {
-    assert_eq!(1, part2(")"));
-    assert_eq!(5, part2("()())"));
+    println!("part1: {}", count);
 }
