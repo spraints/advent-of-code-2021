d83d28d77644764cfaeed76f1ae3e5eec491edf6
Matt Burke <spraints@gmail.com>
2021-12-01 00:17:43 -0500

    part 2

--- src/day1.rs
+++ src/day1.rs
@@ -5,7 +5,7 @@ pub fn run<R: Read>(mut r: R) {
     let nums: Vec<u16> = parse_lines(r);
     let mut last = None;
     let mut count = 0;
-    for num in nums {
+    for num in nums.clone() {
         if let Some(n) = last {
             if num > n {
                 count = count + 1;
@@ -14,4 +14,17 @@ pub fn run<R: Read>(mut r: R) {
         last = Some(num);
     }
     println!("part1: {}", count);
+
+    let mut last3: Vec<u16> = vec![];
+    count = 0;
+    for num in nums.clone() {
+        if last3.len() == 3 {
+            let prev = last3.remove(0);
+            if num > prev {
+                count = count + 1;
+            }
+        }
+        last3.push(num);
+    }
+    println!("part2: {}", count);
 }
