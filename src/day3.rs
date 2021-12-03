fbfc28e5796cbdb2cb90bf8f157e4f7311bfba68
Matt Burke <spraints@gmail.com>
2021-12-03 02:12:54 -0500

    part 2 in rust

--- src/day3.rs
+++ src/day3.rs
@@ -8,6 +8,10 @@ pub fn run<R: Read>(r: R) {
 
     let (gamma, epsilon) = rates(&numbers);
     println!("part 1: {}", gamma * epsilon);
+
+    let o2 = search(&numbers, true);
+    let co2 = search(&numbers, false);
+    println!("part 2: {}", o2 * co2);
 }
 
 fn rates(numbers: &Vec<(Vec<char>, u32)>) -> (u32, u32) {
@@ -27,4 +31,30 @@ fn rates(numbers: &Vec<(Vec<char>, u32)>) -> (u32, u32) {
             (g * 2, e * 2 + 1)
         }
     })
+}
+
+fn search(n: &Vec<(Vec<char>, u32)>, want_most: bool) -> u32 {
+    let mut vals: Vec<(usize, u32)> = n.iter().map(|(_, val)| *val).enumerate().collect();
+    let mut i = 0;
+    loop {
+        let (zeros, ones): (Vec<(usize, u32)>, Vec<(usize, u32)>) =
+            vals.into_iter().partition(|(ni, val)| n[*ni].0[i] == '0');
+        if zeros.len() > ones.len() {
+            if want_most {
+                vals = zeros;
+            } else {
+                vals = ones;
+            }
+        } else {
+            if want_most {
+                vals = ones;
+            } else {
+                vals = zeros;
+            }
+        }
+        if vals.len() == 1 {
+            return vals[0].1;
+        }
+        i += 1;
+    }
 }
