3602a0e6aedb86d9cc7e57399c88b1484e19af2e
Matt Burke <spraints@gmail.com>
2021-12-05 11:51:14 -0500

    ruby part 2

--- ruby/day5.rb
+++ ruby/day5.rb
@@ -1,5 +1,6 @@
 def main
   filled = Hash.new(0)
+  skipped = []
   $stdin.readlines.each do |line|
     from, _, to = line.split(" ")
     x1, y1 = from.split(",").map(&:to_i)
@@ -14,10 +15,24 @@ def main
       low.upto(high) do |x|
         filled[[x,y1]] += 1
       end
+    else
+      skipped << [x1, y1, x2, y2]
     end
   end
 
   puts "part 1: #{filled.count { |_, n| n > 1 }}"
+
+  skipped.each do |x1, y1, x2, y2|
+    x1, y1, x2, y2 = x2, y2, x1, y1 if x1 > x2
+    m = (y2 - y1) / (x2 - x1)
+    y = y1
+    x1.upto(x2) do |x|
+      filled[[x,y]] += 1
+      y += m
+    end
+  end
+
+  puts "part 2: #{filled.count { |_, n| n > 1 }}"
 
   #puts "part 1: #{score(*winners.first)}"
   #puts "part 2: #{score(*winners.last)}"
