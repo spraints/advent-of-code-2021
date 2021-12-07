256975a436c1949b92e625dc396fd9a30afdd842
Matt Burke <spraints@gmail.com>
2021-12-07 00:42:28 -0500

    rewrite

--- ruby/day7.rb
+++ ruby/day7.rb
@@ -1,51 +1,46 @@
 def main
   positions = $stdin.read.strip.split(",").map(&:to_i)
-  midpoint = positions.sort[positions.size / 2]
+  puts "part 1: #{mincost(positions, median(positions), Part1.new)}"
+  puts "part 2: #{mincost(positions, mean(positions), Part2.new)}"
+end
 
-  costs = [midpoint - 1, midpoint, midpoint + 1].map { |mp| cost(positions, mp) }
-  puts "part 1: #{costs.min}"
+def median(ns)
+  ns.sort[ns.size/2]
+end
 
-  costs = [midpoint - 1, midpoint].map { |mp| cost2(positions, mp) }
-  if costs[0] > costs[1]
-    last = midpoint
-    last_cost = costs[1]
-    loop do
-      new_point = last + 1
-      new_cost = cost2(positions, new_point)
-      if new_cost > last_cost
-        puts "part 2: #{last_cost}"
-        break
-      end
-      last, last_cost = new_point, new_cost
+def mean(ns)
+  ns.sum / ns.size
+end
+
+def mincost(positions, start, calc)
+  cost = cost(positions, start, calc)
+  cost1 = cost(positions, start+1, calc)
+  n, cost, step = cost1 < cost ? [start+1, cost1, 1] : [start, cost, -1]
+  loop do
+    nn = n + step
+    ncost = cost(positions, nn, calc)
+    if ncost > cost
+      return cost
     end
-  else
-    last = midpoint - 1
-    last_cost = costs[0]
-    loop do
-      new_point = last - 1
-      new_cost = cost2(positions, new_point)
-      if new_cost > last_cost
-        puts "part 2: #{last_cost}"
-        break
-      end
-      last, last_cost = new_point, new_cost
-    end
+    cost = ncost
+    n = nn
   end
 end
 
-def cost(positions, end_point)
-  positions.inject(0) { |total, pos| total + (pos - end_point).abs }
+def cost(positions, pos, calc)
+  positions.map { |x| calc.cost((x-pos).abs) }.sum
 end
 
-def cost2(positions, end_point)
-  #positions.each do |pos|
-  #  p [pos, sumofn((pos - end_point).abs)]
-  #end
-  positions.inject(0) { |total, pos| total + sumofn((pos - end_point).abs) }
+class Part1
+  def cost(dist)
+    dist
+  end
 end
 
-def sumofn(n)
-  n * (n + 1) / 2
+class Part2
+  def cost(dist)
+    dist * (dist + 1) / 2
+  end
 end
 
 main
