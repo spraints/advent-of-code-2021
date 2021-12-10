cd1219f2ff99cfc9cd30d89d2172b4d08d8f3936
Matt Burke <spraints@gmail.com>
2021-12-10 00:12:00 -0500

    part 2 autocomplete

--- ruby/day10.rb
+++ ruby/day10.rb
@@ -1,11 +1,16 @@
 require "set"
 
 def main
+  incomplete = []
   score = 0
   $stdin.readlines.each do |line|
-    score += analyze(line.strip)
+    stack, n = analyze(line.strip)
+    score += n
+    incomplete << stack if n == 0
   end
   puts "part 1: #{score}"
+  incomplete_scores = incomplete.map { |stack| finish_and_score(stack) }.sort
+  puts "part 2: #{incomplete_scores[incomplete_scores.size / 2]}"
 end
 
 CLOSE_OPEN = {
@@ -33,11 +38,26 @@ def analyze(line)
         raise "can't close with #{c} (#{line.inspect})"
       end
       if CLOSE_OPEN.fetch(c) != stack.pop
-        return SCORE.fetch(c)
+        return [stack, SCORE.fetch(c)]
       end
     end
   end
-  0
+  [stack, 0]
+end
+
+AC_SCORE = {
+  "(" => 1,
+  "[" => 2,
+  "{" => 3,
+  "<" => 4,
+}
+
+def finish_and_score(stack)
+  score = 0
+  until stack.empty?
+    score = score*5 + AC_SCORE.fetch(stack.pop)
+  end
+  score
 end
 
 main
