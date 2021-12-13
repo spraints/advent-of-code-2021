8b7612163dcc6ba3102107c33baae4af607142b8
Matt Burke <spraints@gmail.com>
2021-12-12 23:16:40 -0500

    part 2

--- ruby/day12.rb
+++ ruby/day12.rb
@@ -8,19 +8,31 @@ def main
     graph[b] << a
   end
 
-  puts "part 1: #{count_paths(graph, visited: [], p: "start")}"
+  puts "part 1: #{count_paths(graph, p: "start")}"
+  puts "part 2: #{count_paths(graph, p: "start", double: true)}"
 end
 
-def count_paths(graph, visited:, p:)
+def count_paths(graph, p:, visited: Set.new, path: [], double: false)
+  path = path + [p]
   visited = visited + [p] if p =~ /[a-z]/
+
   count = 0
-  (graph[p] - visited).each do |n|
-    if n == "end"
+  graph[p].each do |n|
+    case
+    when n == "end"
+      #puts path.join(",") + ",end"
       count += 1
+    when n == "start"
+      # nothing
+    when visited.include?(n)
+      if double
+        count += count_paths(graph, p: n, visited: visited, path: path)
+      end
     else
-      count += count_paths(graph, visited: visited, p: n)
+      count += count_paths(graph, p: n, visited: visited, path: path, double: double)
     end
   end
+
   count
 end
 
