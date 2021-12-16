dc4ffd5e34d9417714d898e180ae6fc2304c8eda
Matt Burke <spraints@gmail.com>
2021-12-15 23:18:31 -0500

    use A*

--- ruby/day15.rb
+++ ruby/day15.rb
@@ -7,47 +7,44 @@ end
 
 MAX = 999_999_999
 
-def least_cost(grid, from, to)
-  visited = Set.new
-  cost = Hash.new(MAX)
-  cost[from] = 0
-  current = from
-  visit_soon = Set.new
-  until visited.include?(to)
-    #puts "node: #{current.inspect} (cost = #{cost[current].inspect})"
-    current_cost = cost[current]
-    edges(grid, current).each do |node, edge_cost|
-      next if visited.include?(node)
-      cost[node] = [current_cost + edge_cost, cost[node]].min
-      #puts "  - cost of #{node.inspect} is now #{cost[node]}"
-      visit_soon << node
-      p visit_soon: visit_soon
+def least_cost(grid, start, goal)
+  open_set = Set.new
+  open_set << start
+
+  came_from = Hash.new
+
+  g_score = Hash.new(MAX)
+  g_score[start] = 0
+
+  f_score = Hash.new(MAX)
+  f_score[start] = min_cost(start, goal)
+
+  until open_set.empty?
+    current = open_set.sort_by { |node| f_score[node] }.first
+    if current == goal
+      return g_score[current]
+      #return reconstruct_path(came_from, current)
     end
-    puts "after visiting #{current.inspect}"
-    grid.each_with_index do |row, r|
-      row.each_with_index do |_, c|
-        if current == [r,c]
-          printf "%3d* ", cost[[r,c]]
-        elsif visited.include?([r,c])
-          printf "%3dx ", cost[[r,c]]
-        elsif cost.include?([r,c])
-          printf "%3d  ", cost[[r,c]]
-        elsif to == [r,c]
-          printf " > < "
-        else
-          printf "     "
-        end
+
+    open_set.delete(current)
+    edges(grid, current).each do |neighbor, d|
+      tentative_g_score = g_score[current] + d
+      if tentative_g_score < g_score[neighbor]
+        came_from[neighbor] = current
+        g_score[neighbor] = tentative_g_score
+        f_score[neighbor] = tentative_g_score + min_cost(neighbor, goal)
+        open_set << neighbor
       end
-      printf "\n"
     end
-    visited << current
-    p visit_soon: visit_soon
-    raise "boom" if visit_soon.empty?
-    current = visit_soon.sort_by { |n| cost[n] }.first
-    #p visit_soon: visit_soon
-    visit_soon.delete(current)
   end
-  cost[to]
+
+  raise "no path found"
+end
+
+def min_cost(from, to)
+  dc = (to[1] - from[1]).abs
+  dr = (to[0] - from[0]).abs
+  dc + dr
 end
 
 def bottom_right_node(grid)
