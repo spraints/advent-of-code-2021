cd53db8f6dc72c8a4c7c7a673224fd2ad8bf1945
Matt Burke <spraints@gmail.com>
2021-12-18 21:54:29 -0500

    faster, but not correct yet

--- src/day15.rs
+++ src/day15.rs
@@ -1,100 +1,98 @@
 use super::common::read_lines::read_lines;
-use std::collections::{HashMap, HashSet};
-use std::hash::Hash;
 use std::io::Read;
 
 pub fn run<R: Read>(r: R) {
-    let lines = read_lines(r)
+    let grid = read_lines(r)
         .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
-    let (graph, dest) = make_graph(&lines);
-    println!("part 1: {}", least_cost(&graph, dest));
+    println!("part 1: {} (should be 714)", least_cost(&grid));
 }
 
-fn least_cost(graph: &Graph, dest: Node) -> u32 {
-    let mut unvisited: HashSet<Node> = graph.keys().cloned().collect();
+const INF: u32 = 999_999_999;
 
-    let mut tentative_distance = HashMap::new();
-    let mut cur = Node(0, 0);
-    tentative_distance.insert(cur, 0);
+fn least_cost(grid: &Vec<Vec<u32>>) -> u32 {
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let (dest_r, dest_c) = (rows - 1, cols - 1);
+
+    let mut visited = grid
+        .iter()
+        .map(|r| r.iter().map(|_| false).collect())
+        .collect::<Vec<Vec<bool>>>();
+    let mut tentative_distance = grid
+        .iter()
+        .map(|r| r.iter().map(|_| INF).collect())
+        .collect::<Vec<Vec<u32>>>();
+
+    let (mut r, mut c) = (0, 0);
+    tentative_distance[r][c] = 0;
+    let mut dist = 0;
+
+    let (mut est_rows, mut est_cols) = (0, 0);
 
     loop {
-        // println!(
-        //     "visiting: {:?} ({} unvisited nodes out of {})",
-        //     cur,
-        //     unvisited.len(),
-        //     graph.len()
-        // );
+        //println!("visiting {},{} (dist = {})", r, c, dist);
 
-        let dist = *tentative_distance.get(&cur).unwrap();
-        for e in graph.get(&cur).unwrap() {
-            if unvisited.contains(&e.to) {
-                let this_dist = dist + e.cost;
-                let edist = tentative_distance.entry(e.to).or_insert(999_999_999);
-                //println!("  here to {:?} was {} could be {}", e.to, *edist, this_dist);
-                if this_dist < *edist {
-                    //println!("   updated!");
-                    *edist = this_dist;
+        if r + 1 < rows && !visited[r + 1][c] {
+            let this_dist = dist + grid[r + 1][c];
+            if tentative_distance[r + 1][c] > this_dist {
+                //println!(" ... update {},{} dist to {}", r + 1, c, this_dist);
+                tentative_distance[r + 1][c] = this_dist;
+            }
+        }
+        if c + 1 < cols && !visited[r][c + 1] {
+            let this_dist = dist + grid[r][c + 1];
+            if tentative_distance[r][c + 1] > this_dist {
+                //println!(" ... update {},{} dist to {}", r, c + 1, this_dist);
+                tentative_distance[r][c + 1] = this_dist;
+            }
+        }
+
+        if r + 1 >= est_rows {
+            est_rows = r + 2;
+        }
+        if c + 1 >= est_cols {
+            est_cols = c + 2;
+        }
+
+        visited[r][c] = true;
+
+        if visited[dest_r][dest_c] {
+            // let mut unvisited = 0;
+            // for i in 0..rows {
+            //     for j in 0..cols {
+            //         if !visited[r][c] {
+            //             unvisited += 1;
+            //         }
+            //     }
+            // }
+            // println!("done! {} unvisited nodes", unvisited);
+            return tentative_distance[dest_r][dest_c];
+        }
+
+        //println!("  valid space {},{}", est_rows, est_cols);
+        let (mut next_r, mut next_c) = (0, 0);
+        let mut next_dist = INF;
+        for i in 0..est_rows {
+            if i < rows {
+                for j in 0..est_cols {
+                    if j < cols {
+                        if !visited[i][j] {
+                            let this_dist = tentative_distance[i][j];
+                            //println!("  should i visit {},{} (dist = {}) next?", i, j, this_dist);
+                            if this_dist < next_dist {
+                                next_dist = this_dist;
+                                next_r = i;
+                                next_c = j;
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        unvisited.remove(&cur);
-
-        if !unvisited.contains(&dest) {
-            //println!("dest ({:?}) has been visited!!", dest);
-            return *tentative_distance.get(&dest).unwrap();
-        }
-
-        cur = *unvisited
-            .iter()
-            .reduce(|a, b| match tentative_distance.get(a) {
-                None => b,
-                Some(costa) => match tentative_distance.get(b) {
-                    None => a,
-                    Some(costb) => {
-                        if costa < costb {
-                            a
-                        } else {
-                            b
-                        }
-                    }
-                },
-            })
-            .unwrap();
-    }
-}
-
-#[derive(Hash, Eq, PartialEq, Clone, Copy, Debug)]
-struct Node(usize, usize);
-
-type Graph = HashMap<Node, Vec<Edge>>;
-
-struct Edge {
-    to: Node,
-    cost: u32,
-}
-
-type Lines = Vec<Vec<u32>>;
-
-fn make_graph(lines: &Lines) -> (Graph, Node) {
-    let mut res = HashMap::new();
-    for i in 0..lines.len() {
-        for j in 0..lines[0].len() {
-            let node = Node(i, j);
-            let mut edges = vec![];
-            add_edge(&mut edges, Node(i + 1, j), lines);
-            add_edge(&mut edges, Node(i, j + 1), lines);
-            res.insert(node, edges);
-        }
-    }
-    (res, Node(lines.len() - 1, lines[0].len() - 1))
-}
-
-fn add_edge(edges: &mut Vec<Edge>, to: Node, lines: &Lines) {
-    if let Some(line) = lines.get(to.0) {
-        if let Some(cost) = line.get(to.1) {
-            edges.push(Edge { to, cost: *cost });
-        }
+        r = next_r;
+        c = next_c;
+        dist = next_dist;
     }
 }
