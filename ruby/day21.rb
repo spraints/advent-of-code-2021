9f160acb16315308f731f957c97f750ff451d2f6
Matt Burke <spraints@gmail.com>
2021-12-21 19:52:58 -0500

    day 21!

--- ruby/day21.rb
+++ ruby/day21.rb
@@ -3,49 +3,68 @@ require "set"
 def main
   one, two = $stdin.readlines.map { parse_position _1 }
 
-  #rolls, scores = play(positions: [one, two], die: InOrderDie.new(sides: 100), goal: 1000)
-  #puts "part 1: #{rolls * scores.min}"
+  rolls, scores = play(positions: [one, two], die: InOrderDie.new(sides: 100), goal: 1000)
+  puts "part 1: #{rolls * scores.min}"
 
   wins_per_player = play2([one, two])
   puts "part 2: #{wins_per_player.max}"
 end
 
 def play2(positions)
-  roll_universes = []
-  roll_turns 3, [[]]
-  roll_turns2 3, {{positions: positions, scores: positions.map { 0 }, roll_sum: 0} => 1}
-
-  positions # todo
-end
-
-def roll_turns2(max_rolls, universes)
-  max_rolls.times do |i|
-    universes = roll_turn2(universes)
-    puts "After turn #{i+1}:", universes.map(&:inspect)
+  # p positions: positions
+  universes = {
+    {positions: positions, scores: positions.map { 0 }, roll_sum: 0} => 1,
+  }
+  winners = {0 => 0, 1 => 0}
+  turn_no = 0
+  player = 0
+  until universes.empty?
+    turn_no += 1
+    puts "TURN #{turn_no}"
+    won, universes = play_turn(universes, player)
+    winners[player] += won
+    p winners: winners, n_universes: universes.size
+    #break if turn_no >= 10
+    player = 1 - player
   end
+
+  winners.values
 end
 
-def roll_turn2(universes)
-  new_universes = Hash.new(0)
+def play_turn(universes, player)
+  # Roll 3 times!
+  3.times do
+    new_universes = Hash.new(0)
+    # Split the universe on each die roll!
+    (1..3).each do |n|
+      universes.each do |u, c|
+        new_universes[u.merge(roll_sum: u[:roll_sum] + n)] += c
+      end
+    end
+    universes = new_universes
+  end
+  # Move the pawn and score it!
+  won = 0
+  final_universes = Hash.new(0)
   universes.each do |u, c|
-    (1..3).each do |i|
-      new_universes[u.merge(roll_sum: u[:roll_sum] + i)] += c
+    new_u = score(u, player)
+    if new_u[:scores][player] > 20
+      #p winner: new_u, count: c
+      won += c
+    else
+      final_universes[new_u] += c
     end
   end
-  new_universes
+  [won, final_universes]
 end
 
-def roll_turns(max_rolls, universes, roll: 0)
-  return unless roll < max_rolls
-
-  roll += 1
-  new_universes = universes.flat_map { |u| (1..3).map { |r| u + [r] } }
-
-  puts "After roll #{roll}:"
-  #puts new_universes.map { |u| u.join(",") }
-  puts new_universes.map { _1.sum }.tally.sort_by { _1 }.map { |score, count| sprintf "%4d: %3d times", score, count }
-
-  roll_turns(max_rolls, new_universes, roll: roll)
+def score(universe, player)
+  roll_sum = universe[:roll_sum]
+  positions = universe[:positions].dup
+  positions[player] = update_position(positions[player], roll_sum)
+  scores = universe[:scores].dup
+  scores[player] += positions[player]
+  universe.merge(roll_sum: 0, positions: positions, scores: scores)
 end
 
 def parse_position(line)
@@ -60,11 +79,15 @@ def play(positions:, die:, goal:)
   loop do
     rolls += 3
     move = die.roll + die.roll + die.roll
-    positions[i] = 1 + (positions[i] + move - 1) % 10
+    positions[i] = update_position(positions[i], move)
     scores[i] += positions[i]
     return [rolls, scores] if scores[i] >= goal
     i = 1 - i
   end
+end
+
+def update_position(pos, delta)
+  1 + (pos + delta - 1) % 10
 end
 
 class InOrderDie
